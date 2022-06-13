ddab7cec318c5d7c71039317b1e2dc0d346d5b71
ThuanHome <thuan20132000@gmail.com>
2022-06-14 01:05:53 +0700

    Update blog pagination

--- blog/models.py
+++ blog/models.py
@@ -1,44 +1,45 @@
 from ast import arg
+import imp
 from django.urls import reverse
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-
 # Create your models here.
+
+
 class PublishedManager(models.Manager):
-    def get_queryset(self) :
-        return super(PublishedManager,self).get_queryset().filter(status='published')
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
 
 class Post(models.Model):
     STATUS_CHOICE = (
-        ('draft','Draft'),
-        ('published','Published')
+        ('draft', 'Draft'),
+        ('published', 'Published')
     )
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250,unique_for_date='publish')
-    author = models.ForeignKey(User,on_delete=models.CASCADE,
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
                                related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=50,choices=STATUS_CHOICE,default='draft')
-    
+    status = models.CharField(
+        max_length=50, choices=STATUS_CHOICE, default='draft')
+
     objects = models.Manager()
     published = PublishedManager()
-    
+
     class Meta:
         ordering = ('-publish',)
-        
-        
+
     def __str__(self) -> str:
         return self.title
-    
+
     def get_absolute_url(self):
         return reverse('blog:post_detail',
-                       args=[self.publish.year,
-                             self.publish.month,
-                             self.publish.day,
-                             self.slug
-                             ])
+                       args=[
+                           self.slug
+                       ])
--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,29 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
+
 def post_list(request):
     posts = Post.published.all()
-    return render(request,'blog/post/list.html',{
-        'posts':posts
+    paginator = Paginator(posts, 1)
+    page = request.GET.get('page')
+
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request, 'blog/post/list.html', {
+        'posts': posts,
+        'page': page
     })
-    
-def post_detail(request,year,month,day,post):
-    post = get_object_or_404(Post,slug=post,status='published',
-                             publish__year=year,
-                             publish__month=month,
-                             publish__day=day
-                            )
-    
-    return render(request,'blog/post/detail.html',{'posts':post})
+
+
+def post_detail(request, post):
+    post = get_object_or_404(Post, slug=post, status='published',)
+
+    return render(request, 'blog/post/detail.html', {'post': post})
