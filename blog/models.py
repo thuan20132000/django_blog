066704924e0f84152940059c06cdf678c4648597
ThuanHome <thuan20132000@gmail.com>
2022-06-16 00:13:21 +0700

    Update taggit

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,9 @@
 from ast import arg
-import imp
 from django.urls import reverse
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-
+from taggit.managers import TaggableManager
 # Create your models here.
 
 
@@ -31,6 +30,7 @@ class Post(models.Model):
 
     objects = models.Manager()
     published = PublishedManager()
+    tags = TaggableManager()
 
     class Meta:
         ordering = ('-publish',)
@@ -43,3 +43,20 @@ class Post(models.Model):
                        args=[
                            self.slug
                        ])
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class meta:
+        ordering = ('created',)
+
+    def __str__(self) -> str:
+        return f'Comment by {self.name} on {self.post}'
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .forms import CommentForm, EmailPostForm
+from django.core.mail import send_mail
+from taggit.models import Tag
+from django.db.models import Count
 
 # Create your views here.
 
 
-def post_list(request):
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 6
+    template_name = 'blog/post/list.html'
+
+
+def post_list(request, tag_slug=None):
     posts = Post.published.all()
-    paginator = Paginator(posts, 1)
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        posts = posts.filter(tags__in=[tag])
+    paginator = Paginator(posts, 6)
     page = request.GET.get('page')
 
     try:
@@ -19,11 +35,66 @@ def post_list(request):
 
     return render(request, 'blog/post/list.html', {
         'posts': posts,
-        'page': page
+        'page': page,
+        'tag': tag
     })
 
 
 def post_detail(request, post):
     post = get_object_or_404(Post, slug=post, status='published',)
 
-    return render(request, 'blog/post/detail.html', {'post': post})
+    comments = post.comments.filter(active=True)
+    print('comments:: ', post.comments.filter(active=True))
+    new_comment = None
+
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(
+        tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count(
+        'tags')).order_by('-same_tags', '-publish')[:4]
+    return render(request, 'blog/post/detail.html', {
+        'post': post,
+        'comments': comments,
+        'new_comment': new_comment,
+        'comment_form': comment_form,
+        'similar_posts': similar_posts
+    })
+
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
+    try:
+        if request.method == 'POST':
+            print('aa: ', request.body)
+            form = EmailPostForm(request.POST)
+            if form.is_valid():
+                cd = form.cleaned_data
+                post_url = request.build_absolute_uri(post.get_absolute_url())
+                subject = f"{cd['name']} recommends you read {post.title}"
+                message = f"Read {post.title} at {post_url} \n\n {cd['name']} comments: {cd['comments']}"
+                send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+                sent = True
+        else:
+            form = EmailPostForm()
+
+    except ConnectionRefusedError as e:
+        print('error: ', e)
+
+    return render(request, 'blog/post/share.html', {
+        'post': post,
+        'form': form,
+        'sent': sent
+    })
